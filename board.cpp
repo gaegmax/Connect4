8c50c4166d68a4673da9955019360d01e9ecde71
gaegmax <gaegmax@umich.edu>
2016-03-31 19:38:33 -0400

    Update board.cpp

--- board.cpp
+++ board.cpp
@@ -57,7 +57,196 @@ bool Board::inBounds(int row, int col) const {
 }
 
 int Board::piecesInDirection(int row, int col, int dRow, int dCol) const {
+	int count = 0;
+    
+    for (int row = 0; row < NUM_ROWS; ++row) {
+        for (int col = 0; col < NUM_COLS; ++col) {
+            
+            // Check Player 1
+            if (data[row][col] == Player1) {
+                
+                // Check vertical
+                if (((data[row + abs(dRow)][col] == Player1) && inBounds(row + 1, col))) {
+                    if ((data[row + abs(dRow) + 1][col] == Player1) && inBounds(row + 2, col)){
+                        if ((data[row + abs(dRow) + 2][col] == Player1) && inBounds(row + 3, col)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+                
+                // Check right horizontal
+                if (((data[row][col + abs(dCol)] == Player1) && inBounds(row, col + 1))) {
+                    if ((data[row][col + abs(dCol) + 1] == Player1) && inBounds(row, col + 2)){
+                        if ((data[row][col + abs(dCol) + 2] == Player1) && inBounds(row, col + 3)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+                // Check left horizontal
+                if (((data[row][col - abs(dCol)] == Player1) && inBounds(row, col - 1))) {
+                    if ((data[row][col - abs(dCol) - 1] == Player1) && inBounds(row, abs(col) - 2)){
+                        if ((data[row][col - abs(dCol) - 2] == Player1) && inBounds(row, col - 3)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+                // Check middle horizontal
+                if (((data[row][col - abs(dCol)] == Player1) && (data[row][col + abs(dCol)] == Player1) && inBounds(row, col - 1) && inBounds(row, col +1))) {
+                    if (((data[row][col - abs(dCol) - 1] == Player1) && inBounds(row, col - 2)) || ((data[row][col + abs(dCol) + 1] == Player1) && (inBounds(row, col + abs(dCol) + 1)))) {
+                            count = 3;
+                            return count;
+                    }
+                }
+                // Check right diagonal up
+                if (((data[row - abs(dRow)][col + abs(dCol)] == Player1) && inBounds(row - 1, col + 1))) {
+                    if ((data[row - abs(dRow) - 1][col + abs(dCol) + 1] == Player1) && inBounds(row - 2, col + 2)){
+                        if ((data[row - abs(dRow) - 2][col + abs(dCol) + 2] == Player1) && inBounds(row - 3, col + 3)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+                // Check right diagonal down
+                if (((data[row + abs(dRow)][col - abs(dCol)] == Player1) && inBounds(row + 1, col - 1))) {
+                    if ((data[row + abs(dRow) + 1][col - abs(dCol) - 1] == Player1) && inBounds(row + 2, col - 2)){
+                        if ((data[row + abs(dRow) + 2][col - abs(dCol) - 2] == Player1) && inBounds(row + 3, col - 3)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+                // Check right diagonal middle
+                if (((data[row - abs(dRow)][col + abs(dCol)] == Player1) && (data[row + abs(dRow)][col - abs(dCol)] == Player1) && inBounds(row - 1, col + 1) && inBounds(row + 1, col - 1))) {
+                    if (((data[row - abs(dRow) - 1][col + abs(dCol) + 1] == Player1) && inBounds(row - 2, col + 2)) || ((data[row + abs(dRow) + 1][col - abs(dCol) - 1] == Player1) && (inBounds(row + 2, col - 2)))) {
+                        count = 3;
+                        return count;
+                    }
+                }
+                // Check left diagonal up
+                if (((data[row - abs(dRow)][col - abs(dCol)] == Player1) && inBounds(row - 1, col - 1))) {
+                    if ((data[row - abs(dRow) - 1][col - abs(dCol) - 1] == Player1) && inBounds(row - 2, col - 2)){
+                        if ((data[row - abs(dRow) - 2][col - abs(dCol) - 2] == Player1) && inBounds(row - 3, col - 3)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+                // Check left diagonal down
+                if (((data[row + abs(dRow)][col + abs(dCol)] == Player1) && inBounds(row + 1, col + 1))) {
+                    if ((data[row + abs(dRow) + 1][col + abs(dCol) + 1] == Player1) && inBounds(row + 2, col + 2)){
+                        if ((data[row + abs(dRow) + 2][col + abs(dCol) + 2] == Player1) && inBounds(row + 3, col + 3)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+
+                // Check left diagonal middle
+                if (((data[row - abs(dRow)][col - abs(dCol)] == Player1) && (data[row + abs(dRow)][col + abs(dCol)] == Player1) && inBounds(row - 1, col - 1) && inBounds(row + 1, col +1))) {
+                    if (((data[row - abs(dRow) - 1][col - abs(dCol) - 1] == Player1) && inBounds(row - 2, col - 2)) || ((data[row + abs(dRow) + 1][col + abs(dCol) + 1] == Player1) && (inBounds(row + 2, col + 2)))) {
+                        count = 3;
+                        return count;
+                    }
+                }
+            }
+            // Check Player 2
+            if (data[row][col] == Player2) {
+                
+                // Check vertical
+                if (((data[row + abs(dRow)][col] == Player2) && inBounds(row + 1, col))) {
+                    if ((data[row + abs(dRow) + 1][col] == Player2) && inBounds(row + 2, col)){
+                        if ((data[row + abs(dRow) + 2][col] == Player2) && inBounds(row + 3, col)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+                
+                // Check right horizontal
+                if (((data[row][col + abs(dCol)] == Player2) && inBounds(row, col + 1))) {
+                    if ((data[row][col + abs(dCol) + 1] == Player2) && inBounds(row, col + 2)){
+                        if ((data[row][col + abs(dCol) + 2] == Player2) && inBounds(row, col + 3)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+                // Check left horizontal
+                if (((data[row][col - abs(dCol)] == Player2) && inBounds(row, col - 1))) {
+                    if ((data[row][col - abs(dCol) - 1] == Player2) && inBounds(row, abs(col) - 2)){
+                        if ((data[row][col - abs(dCol) - 2] == Player2) && inBounds(row, col - 3)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+                // Check middle horizontal
+                if (((data[row][col - abs(dCol)] == Player2) && (data[row][col + abs(dCol)] == Player2) && inBounds(row, col - 1) && inBounds(row, col +1))) {
+                    if (((data[row][col - abs(dCol) - 1] == Player2) && inBounds(row, col - 2)) || ((data[row][col + abs(dCol) + 1] == Player2) && (inBounds(row, col + abs(dCol) + 1)))) {
+                        count = 3;
+                        return count;
+                    }
+                }
+                // Check right diagonal up
+                if (((data[row - abs(dRow)][col + abs(dCol)] == Player2) && inBounds(row - 1, col + 1))) {
+                    if ((data[row - abs(dRow) - 1][col + abs(dCol) + 1] == Player2) && inBounds(row - 2, col + 2)){
+                        if ((data[row - abs(dRow) - 2][col + abs(dCol) + 2] == Player2) && inBounds(row - 3, col + 3)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+                // Check right diagonal down
+                if (((data[row + abs(dRow)][col - abs(dCol)] == Player2) && inBounds(row + 1, col - 1))) {
+                    if ((data[row + abs(dRow) + 1][col - abs(dCol) - 1] == Player2) && inBounds(row + 2, col - 2)){
+                        if ((data[row + abs(dRow) + 2][col - abs(dCol) - 2] == Player2) && inBounds(row + 3, col - 3)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+                // Check right diagonal middle
+                if (((data[row - abs(dRow)][col + abs(dCol)] == Player2) && (data[row + abs(dRow)][col - abs(dCol)] == Player2) && inBounds(row - 1, col + 1) && inBounds(row + 1, col - 1))) {
+                    if (((data[row - abs(dRow) - 1][col + abs(dCol) + 1] == Player2) && inBounds(row - 2, col + 2)) || ((data[row + abs(dRow) + 1][col - abs(dCol) - 1] == Player2) && (inBounds(row + 2, col - 2)))) {
+                        count = 3;
+                        return count;
+                    }
+                }
+                // Check left diagonal up
+                if (((data[row - abs(dRow)][col - abs(dCol)] == Player1) && inBounds(row - 1, col - 1))) {
+                    if ((data[row - abs(dRow) - 1][col - abs(dCol) - 1] == Player2) && inBounds(row - 2, col - 2)){
+                        if ((data[row - abs(dRow) - 2][col - abs(dCol) - 2] == Player2) && inBounds(row - 3, col - 3)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+                // Check left diagonal down
+                if (((data[row + abs(dRow)][col + abs(dCol)] == Player2) && inBounds(row + 1, col + 1))) {
+                    if ((data[row + abs(dRow) + 1][col + abs(dCol) + 1] == Player2) && inBounds(row + 2, col + 2)){
+                        if ((data[row + abs(dRow) + 2][col + abs(dCol) + 2] == Player2) && inBounds(row + 3, col + 3)) {
+                            count = 3;
+                            return count;
+                        }
+                    }
+                }
+                
+                // Check left diagonal middle
+                if (((data[row - abs(dRow)][col - abs(dCol)] == Player2) && (data[row + abs(dRow)][col + abs(dCol)] == Player2) && inBounds(row - 1, col - 1) && inBounds(row + 1, col +1))) {
+                    if (((data[row - abs(dRow) - 1][col - abs(dCol) - 1] == Player2) && inBounds(row - 2, col - 2)) || ((data[row + abs(dRow) + 1][col + abs(dCol) + 1] == Player2) && (inBounds(row + 2, col + 2)))) {
+                        count = 3;
+                        return count;
+                    }
+                }
+            }
+        }
+    }
     return 0;
+}
+
 }
 
 bool Board::isWin(int row, int col) const {
